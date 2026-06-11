b8b70c7148e7588ada29969424c291a0c28276c9
respirarfisio <terciohzs@hotmail.com>
2026-06-10 23:11:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Altere 'respirar-app' para o nome exato do seu repositório no GitHub
 export default defineConfig({
   plugins: [react()],
-  base: '/respirar-app/',
+  base: '/respirar/',
 })
