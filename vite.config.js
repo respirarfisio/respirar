a413e7764bb4cdbe16b2c3d036b9ab0b647946d0
respirarfisio <terciohzs@hotmail.com>
2026-06-10 22:51:13 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Altere 'respirar-app' para o nome exato do seu repositório no GitHub
-export default defineConfig({
-  plugins: [react()],
-  base: '/respirar/',
-})
